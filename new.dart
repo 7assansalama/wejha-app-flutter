0c0e103384d65d84873ab41ff18a9301e9e3021a
7assansalama <engsalama20@gmail.com>
2025-12-04 22:11:16 +0200

    filter and comparison

---
+++ new.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../core/theme/app_colors.dart';
+import '../../widgets/custom_text_field.dart';
+import '../../widgets/primary_button.dart';
+import '../home/main_layout_screen.dart';
+import 'signup_screen.dart';
